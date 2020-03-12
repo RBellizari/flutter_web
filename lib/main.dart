1faa69fb15000b3afe287c5f4896374c630939f1
bellizari <github.bellizari@gmail.com>
2020-03-12 04:34:29 -0300

    Mobile altered text

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Troco.Tech - Dashboard',
       theme: ThemeData(
         // This is the theme of your application.
         //
